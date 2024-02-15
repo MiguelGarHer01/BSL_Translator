f179ad33bade58fdb09c6a24b4537bc9204381bc
miguel <miguelgarher01@gmail.com>
2024-02-15 13:39:21 +0000

    Saving the dataset into a pickle file

--- dataset_creator.py
+++ dataset_creator.py
@@ -12,38 +12,49 @@ DATA_PATH = "./data"
 expressions = []
 labels = []
 
+print("Loading data...")
+
+print("Processing data...")
+
 for expression_dir in os.listdir(DATA_PATH):
     for sample_dir in os.listdir(os.path.join(DATA_PATH, expression_dir)):
         sample = []
-        for time_step_dir in os.listdir(os.path.join(DATA_PATH, expression_dir, sample_dir)):
+        # Loop from 0 to 19, so that all the features are in order.
+        for i in range(20):
             time_step = []
-            # Loop from 0 to 19, so that all the features are in order.
-            for i in range(20):
 
-                filename = os.path.join(DATA_PATH, expression_dir, sample_dir, time_step_dir, str(i), ".jpg")
+            filename = os.path.join(DATA_PATH, expression_dir, sample_dir, str(i) + ".jpg")
 
-                # Load the image and then convert it from BGR to RGB
-                img = cv2.imread(os.path.join(DATA_PATH, expression_dir, sample_dir, time_step_dir, filename))
-                rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            # Load the image and then convert it from BGR to RGB
+            img = cv2.imread(filename)
+            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-                # Process the image to obtain the landmarks position (x,y)
-                results = mp_hands.process(rgb_img)
+            # Process the image to obtain the landmarks position (x,y)
+            results = mp_hands.process(rgb_img)
 
-                # Loop through all the landmarks
-                for hand_landmarks in results.multi_hand_landmarks:
-                    for j in range(len(hand_landmarks.landmark)):
+            # Loop through all the landmarks
+            for hand_landmarks in results.multi_hand_landmarks:
+                for j in range(len(hand_landmarks.landmark)):
 
-                        x = hand_landmarks.landmark[j].x
-                        y = hand_landmarks.landmark[j].y
+                    x = hand_landmarks.landmark[j].x
+                    y = hand_landmarks.landmark[j].y
 
-                        time_step.append(x)
-                        time_step.append(y)
+                    time_step.append(x)
+                    time_step.append(y)
 
-        sample.append(time_step)
+            sample.append(time_step)
     expressions.append(sample)
     labels.append(expression_dir)
 
-print(expressions)
+print("Data processed SUCCESSFULLY!")
+
+print("Saving data...")
+
+f = open("expressions.pkl", "wb")
+pickle.dump({'expressions': expressions, 'labels': labels}, f)
+f.close()
+
+print("Data saved SUCCESSFULLY!")
 
 
 
