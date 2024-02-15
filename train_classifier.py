b289ce8a649ad19275c01351a904ade18de05e95
miguel <miguelgarher01@gmail.com>
2024-02-15 14:38:25 +0000

    Basic program to train the classifier

--- train_classifier.py
+++ train_classifier.py
@@ -1,10 +1,32 @@
 import os
+import tensorflow as tf
 import pickle
 from sklearn.model_selection import train_test_split
-import numpy as np
+from model import get_model
 
-data_dict = pickle.load(open('./expressions.pkl', 'wb'))
+from helper import normalize_expressions, normalize_labels, number_classes
 
-expressions = data_dict['expressions']
-labels = data_dict['labels']
+EPOCHS = 150
+
+data_dict = pickle.load(open('expressions.pickle', 'rb'))
+
+expressions = normalize_expressions(data_dict['expressions'])
+labels = normalize_labels(data_dict['labels'])
+
+num_classes = number_classes(labels)
+
+X_train, X_test, y_train, y_test = train_test_split(expressions, labels)
+
+classifier = get_model(num_classes)
+
+classifier.summary()
+
+classifier.fit(X_train, y_train, epochs=EPOCHS)
+
+classifier.evaluate(X_test, y_test)
+
+predictions = classifier.predict(X_test)
+
+print(predictions)
+
 
