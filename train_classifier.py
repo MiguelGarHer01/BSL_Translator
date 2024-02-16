d74fed5d24bf23d1b4bb108e7d46c58772acb87e
miguel <miguelgarher01@gmail.com>
2024-02-16 13:02:08 +0000

    Added early stopping

--- train_classifier.py
+++ train_classifier.py
@@ -1,8 +1,11 @@
 import os
 import tensorflow as tf
 import pickle
+
+from keras.src.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
 from model import get_model
+from keras import callbacks
 
 from helper import normalize_expressions, normalize_labels, number_classes
 
@@ -19,9 +22,11 @@ X_train, X_test, y_train, y_test = train_test_split(expressions, labels)
 
 classifier = get_model(num_classes)
 
+es = EarlyStopping(monitor='val_loss', mode='min', patience=50, verbose=1)
+
 classifier.summary()
 
-classifier.fit(X_train, y_train, epochs=EPOCHS)
+classifier.fit(X_train, y_train, epochs=EPOCHS, callbacks=[es])
 
 classifier.evaluate(X_test, y_test)
 
