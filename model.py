fc98851941da4678b65b17c093f1baf7cfac1c58
miguel <miguelgarher01@gmail.com>
2024-02-16 08:50:36 +0000

    Adapted the model in order to increase accuracy

--- model.py
+++ model.py
@@ -8,11 +8,12 @@ Sequential model definition for the expression classifier
 
 def get_model(num_class):
     model = Sequential()
-    model.add(LSTM(32, input_shape=(20, 42), return_sequences=True, activation='relu'))
-    model.add(LSTM(64, return_sequences=True, activation='relu'))
-    model.add(LSTM(32, return_sequences=False, activation='relu'))
+    model.add(LSTM(64, input_shape=(20, 42), return_sequences=True, activation='relu'))
+    model.add(LSTM(128, return_sequences=True, activation='relu'))
+    model.add(LSTM(128, return_sequences=False, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(64, activation='relu'))
     model.add(Dense(32, activation='relu'))
-    model.add(Dense(16, activation='relu'))
     model.add(Dense(num_class, activation='sigmoid'))
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
