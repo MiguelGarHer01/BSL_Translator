d33010f130be2e2999d38bc15bb8afbd7b292ef1
miguel <miguelgarher01@gmail.com>
2024-02-28 13:42:56 +0000

    Implementing a progress bar to see how images are processed

--- dataset_creator.py
+++ dataset_creator.py
@@ -3,6 +3,8 @@ import mediapipe as mp
 import pickle
 import os
 
+from alive_progress import alive_bar
+
 # Import the mediapipe model for hand landmarks detection. Static images
 mp_hands = mp.solutions.hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
@@ -19,58 +21,32 @@ print("Processing data...")
 for expression_dir in os.listdir(DATA_PATH):
     for sample_dir in os.listdir(os.path.join(DATA_PATH, expression_dir)):
         sample = []
-        # Loop from 0 to 19, so that all the features are in order.
-        for i in range(20):
-            time_step = []
 
-            # Added two lists to normalize the position of the hand in the frame
-            norm_x = []
-            norm_y = []
-            count = 0
+        with alive_bar(20) as bar:
+            # Loop from 0 to 19, so that all the features are in order.
+            for i in range(20):
+                time_step = []
 
-            filename = os.path.join(DATA_PATH, expression_dir, sample_dir, str(i) + ".jpg")
+                # Added two lists to normalize the position of the hand in the frame
+                norm_x = []
+                norm_y = []
+                count = 0
 
-            # Load the image and then convert it from BGR to RGB
-            img = cv2.imread(filename)
-            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                filename = os.path.join(DATA_PATH, expression_dir, sample_dir, str(i) + ".jpg")
 
-            # Process the image to obtain the landmarks position (x,y)
-            results = mp_hands.process(rgb_img)
+                # Load the image and then convert it from BGR to RGB
+                img = cv2.imread(filename)
+                rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-            # Loop through all the landmarks
-            for hand_landmarks in results.multi_hand_landmarks:
-                if len(results.multi_handedness) == 1:
+                # Process the image to obtain the landmarks position (x,y)
+                results = mp_hands.process(rgb_img)
 
-                    for j in range(len(hand_landmarks.landmark)):
-
-                        x = hand_landmarks.landmark[j].x
-                        y = hand_landmarks.landmark[j].y
-
-                        # Save the coordinates for a future normalization
-                        norm_x.append(x)
-                        norm_y.append(y)
-
-                    for j in range(len(hand_landmarks.landmark)):
-
-                        x = hand_landmarks.landmark[j].x
-                        y = hand_landmarks.landmark[j].y
-
-                        # NORMALIZATION
-                        x = x - min(norm_x)
-                        y = y - min(norm_y)
-
-                        # Save the coordinates as features in our time_step
-                        time_step.append(x)
-                        time_step.append(y)
-
-                    # Save the time steps into our sample expression
-                    sample.append(time_step)
-
-                else:
-
-                    if count < 21:
+                # Loop through all the landmarks
+                for hand_landmarks in results.multi_hand_landmarks:
+                    if len(results.multi_handedness) == 1:
 
                         for j in range(len(hand_landmarks.landmark)):
+
                             x = hand_landmarks.landmark[j].x
                             y = hand_landmarks.landmark[j].y
 
@@ -91,10 +67,40 @@ for expression_dir in os.listdir(DATA_PATH):
                             time_step.append(x)
                             time_step.append(y)
 
-                            count += 1
-
                         # Save the time steps into our sample expression
                         sample.append(time_step)
+
+                    else:
+
+                        if count < 21:
+
+                            for j in range(len(hand_landmarks.landmark)):
+                                x = hand_landmarks.landmark[j].x
+                                y = hand_landmarks.landmark[j].y
+
+                                # Save the coordinates for a future normalization
+                                norm_x.append(x)
+                                norm_y.append(y)
+
+                            for j in range(len(hand_landmarks.landmark)):
+
+                                x = hand_landmarks.landmark[j].x
+                                y = hand_landmarks.landmark[j].y
+
+                                # NORMALIZATION
+                                x = x - min(norm_x)
+                                y = y - min(norm_y)
+
+                                # Save the coordinates as features in our time_step
+                                time_step.append(x)
+                                time_step.append(y)
+
+                                count += 1
+
+                            # Save the time steps into our sample expression
+                            sample.append(time_step)
+
+                bar()
 
         # Save the sample of the expression into the expression list
         expressions.append(sample)
